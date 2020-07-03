ba5b7262b485d5d0ac3e02ed420fb9ee496f5a6c
Jitgitbit <rowowolf@gmail.com>
2020-07-03 20:57:12 +0200

    Using the Device camera

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as path;
-import 'package:path_provider/path_provider.dart' as syspaths;
+// import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   @override
@@ -20,11 +20,11 @@ class _ImageInputState extends State<ImageInput> {
       maxWidth: 600,
     );
     setState(() {
-      _storedImage = imageFile;
+      // _storedImage = imageFile;
     });
-    final appDir = await syspaths.getApplicationDocumentsDirectory();
-    final fileName = path.basename(imageFile.path);
-    final savedImage = await imageFile.copy('${appDir.path}/$fileName');
+    // final appDir = await syspaths.getApplicationDocumentsDirectory();
+    // final fileName = path.basename(imageFile.path);
+    // final savedImage = await imageFile.copy('${appDir.path}/$fileName');
   }
 
   @override
