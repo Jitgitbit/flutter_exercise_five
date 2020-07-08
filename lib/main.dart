5b7a112270253e6d82c931fe677b703a007f8fa9
Jitgitbit <rowowolf@gmail.com>
2020-07-08 10:35:29 +0200

    Adding a "Place Detail" Screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:flutter_config/flutter_config.dart';
 import './providers/great_places.dart';
 import './screens/places_list_screen.dart';
 import './screens/add_place_screen.dart';
+import './screens/place_detail_screen.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,16 +24,16 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider.value(
       value: GreatPlaces(),
       child: MaterialApp(
-        title: 'PhoenixFlutterPlaces',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          accentColor: Colors.amber,
-        ),
-        home: PlacesListScreen(),
-        routes: {
-          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
-        },
-      ),
+          title: 'PhoenixFlutterPlaces',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            accentColor: Colors.amber,
+          ),
+          home: PlacesListScreen(),
+          routes: {
+            AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+            PlaceDetailScreen.routeName: (ctx) => PlaceDetailScreen(),
+          }),
     );
   }
 }
